097cd5c01ddcc28933de38a85191922bbf06db75
InpossibleMomo <momo.ylinen@outlook.com>
2022-03-10 14:53:14 +0200

    Game logic improvements

--- screens/Game.js
+++ screens/Game.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View, SafeAreaView, FlatList } from "react-native";
 import React, { useState } from "react";
 import Game from "../components/game";
 import lisaaPelaaja from "../questions/testquestions";
+import getRandomInt from "../utils/getRandom";
 
 const GamePlay = ({ navigation, route }) => {
   const [selected, setSelected] = useState();
@@ -10,22 +11,23 @@ const GamePlay = ({ navigation, route }) => {
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
     21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34,
   ]);
+  const [startNewGame, setStartNewGame] = useState(true);
 
   const players = navigation.getParam("gamers");
 
-  console.log("Kysyysnumero: ", questionNumber[counter], " Laskuri: ", counter);
   const handlerandomClick = () => {
     if (players.length <= 2) {
       setSelected("Lisää pelaajia");
       return;
     }
 
-    if (counter < 2) {
+    if (counter === 0) {
       let randomize = questionNumber
         .map((value) => ({ value, sort: Math.random() }))
         .sort((a, b) => a.sort - b.sort)
         .map(({ value }) => value);
       setQuestionNumber(randomize);
+      console.log("randomize", randomize);
 
       let randomPlayerlist = players
         .map((value) => ({ value, sort: Math.random() }))
@@ -39,7 +41,7 @@ const GamePlay = ({ navigation, route }) => {
       return;
     }
     console.log(
-      "Kysyysnumero: ",
+      "Kysymysnumero: ",
       questionNumber[counter],
       " Laskuri: ",
       counter
@@ -53,14 +55,38 @@ const GamePlay = ({ navigation, route }) => {
     const randomPlayerOne = randomPlayerlist[0].player;
     const randomPlayerTwo = randomPlayerlist[1].player;
     console.log(randomPlayerOne, randomPlayerTwo);
-    const kysymys = lisaaPelaaja(
-      randomPlayerOne,
-      randomPlayerTwo,
-      questionNumber[counter]
-    );
-    setSelected(kysymys);
-    setCounter(counter + 1);
+    if (counter === 0) {
+      const randomInt = getRandomInt(0, 30);
+      const kysymys = lisaaPelaaja(
+        randomPlayerOne,
+        randomPlayerTwo,
+        questionNumber[randomInt]
+      );
+      console.log("Täällä", randomInt, kysymys);
+
+      setSelected(kysymys);
+      setCounter(counter + 1);
+    } else {
+      const kysymys = lisaaPelaaja(
+        randomPlayerOne,
+        randomPlayerTwo,
+        questionNumber[counter]
+      );
+      setSelected(kysymys);
+      setCounter(counter + 1);
+    }
   };
+
+  if (startNewGame === true) {
+    setStartNewGame(false);
+    let randomize = questionNumber
+      .map((value) => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value);
+    setQuestionNumber(randomize);
+    console.log("randomize", randomize);
+    handlerandomClick();
+  }
 
   return (
     <Game
